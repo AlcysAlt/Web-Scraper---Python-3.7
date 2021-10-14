3a97bf72b8ae2ec189223546596752c7801a3a47
AlcysAlt <awesomealcys@gmail.com>
2021-10-14 12:57:45 +0100

    WORKINGSCRAPER

--- Web_Scraper___Python_3.7.py
+++ Web_Scraper___Python_3.7.py
@@ -3,8 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
+import time
 
-def findPledged(page):
+def getPledged(page):
     soup = BeautifulSoup(page, 'html.parser')
     
     for span in soup.findAll("span", {"data-test-id": "amount-pledged"}):
@@ -19,7 +20,17 @@ def scraper(url):
 def interface():
     print("Art - 1\nTechnology - 16\n ")
     option = input("Please select a category:")
-    url = "https://www.kickstarter.com/discover/advanced?category_id=" + option + "&sort=newest&seed=2723668&page=1"
+    url = "https://www.kickstarter.com/discover/advanced?category_id=" + option + "&sort=newest&seed=2723668&page="
+    maxPages = input("Please enter the amount of pages to scrape: ")
+
+    for int(i) in range(1, maxPages):
+        tempUrl = url + i
+        print(tempURL)
+        #getPledged(scraper(tempUrl))
+        #time.sleep(1)
+    
     print(url)
-    findPledged(scraper(url))
+    getPledged(scraper(url))
+
 interface()
+
