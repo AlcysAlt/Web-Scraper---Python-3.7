5076bea522c6049206d6adbb38699edd975ba7ff
AlcysAlt <awesomealcys@gmail.com>
2021-10-14 10:28:58 +0100

    scraperv2

--- Web_Scraper___Python_3.7.py
+++ Web_Scraper___Python_3.7.py
@@ -1,1 +1,7 @@
-xddddd
+from selenium import webdriver
+def scraper(url):
+
+def interface():
+    print("Art - 1\nTechnology - 16\n ")
+    option = input("Please select a category:")
+    url = "https://www.kickstarter.com/discover/advanced?category_id=" + option + "&sort=newest&seed=2723668&page=1"
