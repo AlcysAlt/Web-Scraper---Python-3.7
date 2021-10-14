88f3915d2d98cce70d97ba44ec92eae331e5ecd7
AlcysAlt <awesomealcys@gmail.com>
2021-10-14 13:51:06 +0100

    Working

--- Web_Scraper___Python_3.7.py
+++ Web_Scraper___Python_3.7.py
@@ -3,6 +3,7 @@ from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 import time
 from price_parser import Price
+import requests
 
 
 def getPledged(page):
@@ -11,29 +12,30 @@ def getPledged(page):
     for span in soup.findAll("span", {"data-test-id": "amount-pledged"}):
         pledge = Price.fromstring(span.text)
         totalPledged = totalPledged + pledge.amount_float
-    print(totalPledged)
+    return totalPledged
 
-    
-def scraper(url):
+
+def scraper1(url):
     browser = webdriver.Chrome()
     browser.get(url)
 
     page = browser.page_source
     return page
 
+
 def interface():
     print("Art - 1\nTechnology - 16\n ")
     option = input("Please select a category: ")
     url = str("https://www.kickstarter.com/discover/advanced?category_id=" + option + "&sort=newest&seed=2723668&page=")
     maxPages = int(input("Please enter the amount of pages to scrape: "))
-
+    totalPledged = 0.0
     for i in range(1, maxPages + 1):
         tempUrl = url + str(i)
         print(tempUrl)
-        getPledged(scraper(tempUrl))
+        totalPledged = totalPledged + getPledged(scraper1(tempUrl))
         print("Next Page")
         time.sleep(1)
-    
+    print("Total amount pledged:", totalPledged)
 
 
 interface()
